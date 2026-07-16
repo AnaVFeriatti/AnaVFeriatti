d238e6cb27e975f07609ec7e61e80f3836b7f21d
AnaVFeriatti <vitoria.feriatti@gmail.com>
2026-07-16 00:28:34 -0300

    certificacoes

--- docs/js/certs-data.js
+++ docs/js/certs-data.js
@@ -9,10 +9,24 @@ const CERTS_DATA = [
     url: '',
   },
   {
+    title: 'BANCO DE DADOS PARA DATA SCIENCE',
+    issuer: 'SENAI Ourinhos',
+    date: '2025',
+    category: 'Certificacao',
+    url: '',
+  },
+  {
+    title: 'MICROSOFT POWER BI',
+    issuer: 'SENAI Ourinhos',
+    date: '2025',
+    category: 'Certificacao',
+    url: '',
+  },
+  {
     title: 'Ferramentas para Ciência de Dados',
     issuer: 'Even3',
     date: '2024',
-    category: 'dados',
+    category: 'Certificacao',
     url: '',
   },
   {
